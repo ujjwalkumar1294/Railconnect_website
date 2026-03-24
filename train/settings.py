33a8e8ef174306da00d9991e7622f1cf28fd6d29
ujjwalkumar1294 <ujjwalkumar1294309@gmail.com>
2026-03-24 22:46:13 +0530

    main changes

--- train/settings.py
+++ train/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^uhqhxqlz5d32&0j&v#ejgp7rwd)=h^2ngbu#41ch5&f&k&k#r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'adminpanel',
     'trains',
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
